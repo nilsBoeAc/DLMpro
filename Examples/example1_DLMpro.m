b5d0b8dc71ff6dd0de5d2602639a10c2aabf9994
NilsB <nils.boehnisch@gmx.de>
2022-04-14 16:53:58 +0200

    Add Example Files

---
+++ Examples/example1_DLMpro.m
@@ -0,0 +1,35 @@
+
+% Example 1 
+% 
+%% Description
+% This script, contains an example for DLMpro.
+% It contains:
+%   - define wing and create object
+%   - calc AIC with various Methods
+%   - access results
+%
+%% Note
+%   Please make sure, that DLMpro is installed.
+%
+%% Disclaimer
+%   Copyright (c) 2021 Nils Böhnisch, Marc Bangel.
+%
+%   This file is part of DLMPro.
+%
+%   DLMPro is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation, either version 3 of the License, or
+%   any later version. Also see the file "License"
+
+%% Initialize
+clearvars;
+
+%% Wing definition
+
+%% Object creation
+
+%% Calc AIC with given red. frequencies and mach numbers
+
+%% Adjust Method (Integration and Approximation)
+
+%% Access results
